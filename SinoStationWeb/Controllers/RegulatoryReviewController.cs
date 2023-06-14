864e145e19ebcc2e0513804dc5ac2c0d6193a60d
DonaldLu <lugenius2001@gmail.com>
2023-06-14 16:36:01 +0800

    撰寫房間校核Web頁面

---
+++ SinoStationWeb/Controllers/RegulatoryReviewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinoStationWeb.Controllers
+{
+    public class RegulatoryReviewController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Room()
+        {
+            return View();
+        }
+    }
+}
