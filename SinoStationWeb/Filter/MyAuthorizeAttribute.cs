8a07f0907a5ff692b4e93017defdcc4e84c3a205
DonaldLu <lugenius2001@gmail.com>
2023-06-14 15:59:57 +0800

    Original

---
+++ SinoStationWeb/Filter/MyAuthorizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinoStationWeb.Filter
+{
+    public class MyAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (!httpContext.User.Identity.IsAuthenticated)
+                return false;
+            return true;
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+
+            if (filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.HttpContext.Response.Redirect("~/Error/Unauthorized/" +
+                filterContext.HttpContext.User.Identity.Name);
+                filterContext.Result = new EmptyResult();
+                return;
+            }
+        }
+    }
+}
